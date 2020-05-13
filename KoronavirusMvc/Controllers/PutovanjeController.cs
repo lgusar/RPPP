6ed9fd058106765b7fd3b31aefafc06a864dd775
petra <pk119123344@fer.hr>
2020-05-13 23:20:25 +0200

    added some Controllers

---
+++ KoronavirusMvc/Controllers/PutovanjeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KoronavirusMvc.Models;
+
+namespace KoronavirusMvc.Controllers
+{
+    public class PutovanjeController : Controller
+    {
+        private readonly RPPP09Context _context;
+
+        public PutovanjeController(RPPP09Context context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+            var putovanja = _context.Putovanje
+                                    .AsNoTracking()
+                                    .ToList();
+            return View(putovanja);
+        }
+    }
+}
