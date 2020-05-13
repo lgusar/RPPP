07d79b6103d10d8e40badf6a90070005c0824363
Goran Horvat <goran.horvat@fer.hr>
2020-05-13 05:38:35 +0200

    Dodan Stozer i klijentsko stranicenje

---
+++ KoronavirusMvc/Controllers/StozerController.cs
@@ -0,0 +1,28 @@
+﻿using KoronavirusMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace KoronavirusMvc.Controllers
+{
+    public class StozerController : Controller
+    {
+
+        private readonly RPPP09Context ctx;
+
+        public StozerController(RPPP09Context ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var stozeri = ctx.Stozer
+                .AsNoTracking()
+                .OrderBy(d => d.SifraStozera).
+                ToList();
+
+            return View(stozeri);
+        }
+    }
+}
