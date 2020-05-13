55cab7f90d365dabece8fd3b0fd366b199d6ca71
Lovre Gusar <lovreg79@gmail.com>
2020-05-13 23:45:45 +0200

    Dodan pravilan serverski ispis popisa simptoma

--- KoronavirusMvc/Controllers/SimptomController.cs
+++ KoronavirusMvc/Controllers/SimptomController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using KoronavirusMvc.Extensions;
 using KoronavirusMvc.Models;
+using KoronavirusMvc.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace KoronavirusMvc.Controllers
 {
@@ -12,17 +13,101 @@ namespace KoronavirusMvc.Controllers
     {
         private readonly RPPP09Context ctx;
 
-        public SimptomController(RPPP09Context ctx)
+        private readonly AppSettings appSettings;
+
+        public SimptomController(RPPP09Context ctx, IOptionsSnapshot<AppSettings> optionsSnapshot)
         {
             this.ctx = ctx;
-        } 
+            appSettings = optionsSnapshot.Value;
+        }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1, int sort = 1, bool ascending = true)
         {
-            var simptomi = ctx.Simptom
-                              .AsNoTracking()
+            int pagesize = appSettings.PageSize;
+            var query = ctx.Simptom.AsNoTracking();
+
+            int count = query.Count();
+
+            var pagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                Sort = sort,
+                Ascending = ascending,
+                ItemsPerPage = pagesize,
+                TotalItems = count
+            };
+
+            if (page > pagingInfo.TotalPages)
+            {
+                return RedirectToAction(nameof(Index), new
+                {
+                    page = pagingInfo.TotalPages,
+                    sort,
+                    ascending
+                });
+            }
+
+            System.Linq.Expressions.Expression<Func<Simptom, object>> orderSelector = null;
+            switch (sort)
+            {
+                case 1:
+                    orderSelector = s => s.SifraSimptoma;
+                    break;
+                case 2:
+                    orderSelector = s => s.Opis;
+                    break;
+            }
+
+            if (orderSelector != null)
+            {
+                query = ascending ? query.OrderBy(orderSelector) : query.OrderByDescending(orderSelector);
+            }
+
+            var simptomi = query
+                              .Skip((page - 1) * pagesize)
+                              .Take(pagesize)
                               .ToList();
-            return View(simptomi);
+
+            var model = new SimptomiViewModel
+            {
+                Simptomi = simptomi,
+                PagingInfo = pagingInfo
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Simptom simptom)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ctx.Add(simptom);
+                    ctx.SaveChanges();
+
+                    TempData[Constants.Message] = $"Pregled {simptom.SifraSimptoma} uspješno dodan.";
+                    TempData[Constants.ErrorOccured] = false;
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception exc)
+                {
+                    ModelState.AddModelError(string.Empty, exc.CompleteExceptionMessage());
+                    return View(simptom);
+                }
+            }
+            else
+            {
+                return View(simptom);
+            }
         }
     }
 }
