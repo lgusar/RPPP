c5e6e07edbf1b79a24d56174d719b3a5c4a57c07
Magda <magda_1999@hotmail.com>
2020-05-28 13:56:52 +0200

    dodan partial view za zarazenu osobu

--- KoronavirusMvc/Controllers/ZarazenaOsobaController.cs
+++ KoronavirusMvc/Controllers/ZarazenaOsobaController.cs
@@ -23,148 +23,148 @@ namespace KoronavirusMvc.Controllers
 
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Create()
-        {
-            await PrepareDropdownLists();
-            return View();
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> Create()
+        //{
+        //    await PrepareDropdownLists();
+        //    return View();
+        //}
 
-        private async Task PrepareDropdownLists()
-        {
-            var stanja = await ctx.Stanje.OrderBy(s => s.NazivStanja).Select(s => new { s.NazivStanja, s.SifraStanja }).ToListAsync();
-            ViewBag.Stanja = new SelectList(stanja, nameof(Stanje.SifraStanja), nameof(Stanje.NazivStanja));
-        }
+        //private async Task PrepareDropdownLists()
+        //{
+        //    var stanja = await ctx.Stanje.OrderBy(s => s.NazivStanja).Select(s => new { s.NazivStanja, s.SifraStanja }).ToListAsync();
+        //    ViewBag.Stanja = new SelectList(stanja, nameof(Stanje.SifraStanja), nameof(Stanje.NazivStanja));
+        //}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(ZarazenaOsoba zarazenaOsoba)
-        {
-            if (ModelState.IsValid)
-            {
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(ZarazenaOsoba zarazenaOsoba)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
 
-                try
-                {
-                    ctx.Add(zarazenaOsoba);
-                    ctx.SaveChanges();
-                    TempData[Constants.Message] = $"Osoba {zarazenaOsoba.IdentifikacijskiBroj} uspješno dodana u listu zaraženih osoba. ";
-                    TempData[Constants.ErrorOccurred] = false;
+        //        try
+        //        {
+        //            ctx.Add(zarazenaOsoba);
+        //            ctx.SaveChanges();
+        //            TempData[Constants.Message] = $"Osoba {zarazenaOsoba.IdentifikacijskiBroj} uspješno dodana u listu zaraženih osoba. ";
+        //            TempData[Constants.ErrorOccurred] = false;
 
-                    return RedirectToAction(nameof(Index));
-                }
-                catch (Exception exc)
-                {
-                    ModelState.AddModelError(string.Empty, exc.CompleteExceptionMessage());
-                    await PrepareDropdownLists();
-                    return View(zarazenaOsoba);
-                }
-            }
-            else
-            {
-                await PrepareDropdownLists();
-                return View(zarazenaOsoba);
-            }
-        }
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch (Exception exc)
+        //        {
+        //            ModelState.AddModelError(string.Empty, exc.CompleteExceptionMessage());
+        //            await PrepareDropdownLists();
+        //            return View(zarazenaOsoba);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        await PrepareDropdownLists();
+        //        return View(zarazenaOsoba);
+        //    }
+        //}
 
-        [HttpGet]
-        public async Task<IActionResult> Edit(string id, int page = 1, int sort = 1, bool ascending = true)
-        {
-            var zarazenaOsoba = await ctx.ZarazenaOsoba.FindAsync(id);
-            if (zarazenaOsoba == null)
-            {
-                return NotFound($"Ne postoji osoba s identifikacijskim brojem {id}");
-            }
-            else
-            {
-                ViewBag.Page = page;
-                ViewBag.Sort = sort;
-                ViewBag.Ascending = ascending;
-                await PrepareDropdownLists();
+        //[HttpGet]
+        //public async Task<IActionResult> Edit(string id, int page = 1, int sort = 1, bool ascending = true)
+        //{
+        //    var zarazenaOsoba = await ctx.ZarazenaOsoba.FindAsync(id);
+        //    if (zarazenaOsoba == null)
+        //    {
+        //        return NotFound($"Ne postoji osoba s identifikacijskim brojem {id}");
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Page = page;
+        //        ViewBag.Sort = sort;
+        //        ViewBag.Ascending = ascending;
+        //        await PrepareDropdownLists();
                 
-                return View(zarazenaOsoba);
-            }
-        }
+        //        return View(zarazenaOsoba);
+        //    }
+        //}
 
-        [HttpPost, ActionName("Edit")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Update(string id, int page = 1, int sort = 1, bool ascending = true)
-        {
-            try
-            {
-                ZarazenaOsoba zarazenaOsoba = await ctx.ZarazenaOsoba.FindAsync(id);
-                if (zarazenaOsoba == null)
-                {
-                    return NotFound($"Ne postoji osoba s identifikacijskim brojem {id}");
-                }
+        //[HttpPost, ActionName("Edit")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Update(string id, int page = 1, int sort = 1, bool ascending = true)
+        //{
+        //    try
+        //    {
+        //        ZarazenaOsoba zarazenaOsoba = await ctx.ZarazenaOsoba.FindAsync(id);
+        //        if (zarazenaOsoba == null)
+        //        {
+        //            return NotFound($"Ne postoji osoba s identifikacijskim brojem {id}");
+        //        }
 
-                ViewBag.Page = page;
-                ViewBag.Sort = sort;
-                ViewBag.Ascending = ascending;
-                bool ok = await TryUpdateModelAsync<ZarazenaOsoba>(zarazenaOsoba, "", z => z.DatZaraze, z => z.SifraStanja);
-                if (ok)
-                {
-                    try
-                    {
+        //        ViewBag.Page = page;
+        //        ViewBag.Sort = sort;
+        //        ViewBag.Ascending = ascending;
+        //        bool ok = await TryUpdateModelAsync<ZarazenaOsoba>(zarazenaOsoba, "", z => z.DatZaraze, z => z.SifraStanja);
+        //        if (ok)
+        //        {
+        //            try
+        //            {
                         
-                        TempData[Constants.Message] = $"Podaci osobe  uspješno ažurirani.";
-                        TempData[Constants.ErrorOccurred] = false;
-                        await ctx.SaveChangesAsync();
-                        return RedirectToAction(nameof(Index), new { page, sort, ascending });
-                    }
-                    catch (Exception exc)
-                    {
-                        ModelState.AddModelError(string.Empty, exc.CompleteExceptionMessage());
-                        await PrepareDropdownLists();
-                        return View(zarazenaOsoba);
-                    }
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Podatke o osobi nije moguće povezati s forme");
-                    await PrepareDropdownLists();
-                    return View(zarazenaOsoba);
-                }
-            }
-            catch (Exception exc)
-            {
-                TempData[Constants.Message] = exc.CompleteExceptionMessage();
-                TempData[Constants.ErrorOccurred] = true;
-                return RedirectToAction(nameof(Edit), new { id, page, sort, ascending });
-            }
-        }
+        //                TempData[Constants.Message] = $"Podaci osobe  uspješno ažurirani.";
+        //                TempData[Constants.ErrorOccurred] = false;
+        //                await ctx.SaveChangesAsync();
+        //                return RedirectToAction(nameof(Index), new { page, sort, ascending });
+        //            }
+        //            catch (Exception exc)
+        //            {
+        //                ModelState.AddModelError(string.Empty, exc.CompleteExceptionMessage());
+        //                await PrepareDropdownLists();
+        //                return View(zarazenaOsoba);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            ModelState.AddModelError(string.Empty, "Podatke o osobi nije moguće povezati s forme");
+        //            await PrepareDropdownLists();
+        //            return View(zarazenaOsoba);
+        //        }
+        //    }
+        //    catch (Exception exc)
+        //    {
+        //        TempData[Constants.Message] = exc.CompleteExceptionMessage();
+        //        TempData[Constants.ErrorOccurred] = true;
+        //        return RedirectToAction(nameof(Edit), new { id, page, sort, ascending });
+        //    }
+        //}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Delete(string IdentifikacijskiBroj, int page = 1, int sort = 1, bool ascending = true)
-        {
-            var zarazenaOsoba = ctx.ZarazenaOsoba.Find(IdentifikacijskiBroj);
-            if (zarazenaOsoba == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                try
-                {
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Delete(string IdentifikacijskiBroj, int page = 1, int sort = 1, bool ascending = true)
+        //{
+        //    var zarazenaOsoba = ctx.ZarazenaOsoba.Find(IdentifikacijskiBroj);
+        //    if (zarazenaOsoba == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        try
+        //        {
                     
-                    ctx.Remove(zarazenaOsoba);
-                    ctx.SaveChanges();
-                    TempData[Constants.Message] = $"Osoba uspješno obrisana.";
-                    TempData[Constants.ErrorOccurred] = false;
-                }
-                catch (Exception exc)
-                {
-                    TempData[Constants.Message] = $"Pogreška prilikom brisanja osobe: " + exc.CompleteExceptionMessage();
-                    TempData[Constants.ErrorOccurred] = true;
-                }
-                return RedirectToAction(nameof(Index), new { page, sort, ascending });
-            }
-        }
+        //            ctx.Remove(zarazenaOsoba);
+        //            ctx.SaveChanges();
+        //            TempData[Constants.Message] = $"Osoba uspješno obrisana.";
+        //            TempData[Constants.ErrorOccurred] = false;
+        //        }
+        //        catch (Exception exc)
+        //        {
+        //            TempData[Constants.Message] = $"Pogreška prilikom brisanja osobe: " + exc.CompleteExceptionMessage();
+        //            TempData[Constants.ErrorOccurred] = true;
+        //        }
+        //        return RedirectToAction(nameof(Index), new { page, sort, ascending });
+        //    }
+        //}
 
         public IActionResult Index(int page = 1, int sort = 1, bool ascending = true)
         {
             int pagesize = appSettings.PageSize;
-            var query = ctx.ZarazenaOsoba.Include(z => z.SifraStanjaNavigation).Include(z => z.IdentifikacijskiBrojNavigation).AsNoTracking();
+            var query = ctx.ZarazenaOsoba.AsNoTracking();
 
             int count = query.Count();
 
@@ -208,6 +208,14 @@ namespace KoronavirusMvc.Controllers
             }
 
             var zarazeneOsobe = query
+                            .Select(z => new ZarazenaOsobaViewModel
+                            {
+                                IdentifikacijskiBroj = z.IdentifikacijskiBroj,
+                                Ime = z.IdentifikacijskiBrojNavigation.Ime,
+                                Prezime = z.IdentifikacijskiBrojNavigation.Prezime,
+                                DatZaraze = z.DatZaraze,
+                                NazivStanja = z.SifraStanjaNavigation.NazivStanja
+                            })
                             .Skip((page - 1) * pagesize)
                            .Take(pagesize)
                            .ToList();
